15e82a7063280d73fef274519f7463900561920a
Fico <fseitun@gmail.com>
2021-07-22 12:12:06 -0300

    WIP date picker

--- src/components/dolar/ManipularDolar.js
+++ src/components/dolar/ManipularDolar.js
@@ -1,13 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useMutation, useQueryClient } from 'react-query';
 import { Formik, Form, Field, ErrorMessage } from 'formik'; //traer yup para validar
 import { TextField, Button } from '@material-ui/core';
+import { LocalizationProvider, DesktopDatePicker } from '@material-ui/lab';
+import AdapterDateFns from '@material-ui/lab/AdapterDateFns';
 import PropTypes from 'prop-types';
 
 import { cargarDolar } from 'src/components/API';
 import { eliminarDolar } from 'src/components/API';
 
+function Picker({ date, setDate, field }) {
+  console.log(field);
+  const handleChange = (newDate) => {
+    setDate(newDate);
+    // console.log(`newDate del handler del Picker: ${newDate}`);
+  };
+
+  return (
+    <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <DesktopDatePicker
+        label="Fecha"
+        inputFormat="dd/MM/yyyy"
+        value={date}
+        onChange={handleChange}
+        renderInput={(params) => <TextField {...params} />}
+      />
+    </LocalizationProvider>
+  );
+}
+
 export function ManipularDolar({ idSociedad, selectedRows }) {
+  const [date, setDate] = useState(new Date());
+  // console.log(`variable de date de useState: ${date}`);
   const queryClient = useQueryClient();
 
   const { mutate: mutateCargar } = useMutation(
@@ -30,46 +54,74 @@ export function ManipularDolar({ idSociedad, selectedRows }) {
   );
 
   return (
-    <>
-      <Formik
-        initialValues={{
-          fecha: new Date(),
-          mep: 99,
-          // blue: 99,
-          BCRA: 99
-        }}
-        onSubmit={(values, { setSubmitting }) => {
-          mutateCargar(values);
-          console.log(values);
-          setSubmitting(false); //todo: ver de mandarlo a onSuccess
-        }}
-      >
-        {({ isSubmitting }) => (
-          <Form>
-            <Field as={TextField} type="date" name="fecha" />
-            <ErrorMessage name="fecha" component="div" />
-            <Field as={TextField} type="float" name="mep" />
-            <ErrorMessage name="mep" component="div" />
-            {/* <Field as={TextField} type="float" name="blue" />
-            <ErrorMessage name="blue" component="div" /> */}
-            <Field as={TextField} type="float" name="BCRA" />
-            <ErrorMessage name="BCRA" component="div" />
-            <Button type="submit" disabled={isSubmitting}>
-              Agregar
-            </Button>
-          </Form>
-        )}
-      </Formik>
-      <Button
-        onClick={() => {
-          mutateEliminar(selectedRows);
-        }}
-      >
-        Eliminar
-      </Button>
-    </>
+    <Formik
+      initialValues={{
+        fecha: formatDate(new Date()),
+        mep: '',
+        BCRA: ''
+      }}
+      onSubmit={(values, { setSubmitting }) => {
+        console.log(
+          `el values que manda formik a mutate: ${JSON.stringify(
+            values,
+            null,
+            2
+          )}`
+        );
+        mutateCargar(values);
+        setSubmitting(false);
+      }}
+    >
+      {({ isSubmitting }) => (
+        <Form>
+          <Field
+            component={Picker}
+            label="Fecha"
+            type="date"
+            name="fecha"
+            date={date}
+            setDate={setDate}
+            
+          />
+          <ErrorMessage name="fecha" component="div" />
+          <Field as={TextField} label="BCRA" type="float" name="BCRA" />
+          <ErrorMessage name="BCRA" component="div" />
+          <Field as={TextField} label="MEP" type="float" name="mep" />
+          <ErrorMessage name="mep" component="div" />
+          <Button type="submit" disabled={isSubmitting}>
+            Agregar
+          </Button>
+          <Button
+            onClick={() => {
+              mutateEliminar(selectedRows);
+            }}
+          >
+            Eliminar
+          </Button>
+        </Form>
+      )}
+    </Formik>
   );
 }
+
+function formatDate(date) {
+  var d = new Date(date),
+    month = '' + (d.getMonth() + 1),
+    day = '' + d.getDate(),
+    year = d.getFullYear();
+
+  if (month.length < 2) month = '0' + month;
+  if (day.length < 2) day = '0' + day;
+
+  return [year, month, day].join('-');
+}
+
+Picker.propTypes = {
+  setDate: PropTypes.func.isRequired,
+  date: PropTypes.object.isRequired,
+  field: PropTypes.object.isRequired
+};
+
 ManipularDolar.propTypes = {
   idSociedad: PropTypes.number.isRequired,
   selectedRows: PropTypes.array.isRequired
