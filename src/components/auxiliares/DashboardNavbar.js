d14ca4395db0f62b55b0cbb44e0cb7ed01d0cf8d
fseitun <fseitun@gmail.com>
2021-12-02 17:01:18 -0300

    dropdown rubros

--- src/components/auxiliares/DashboardNavbar.js
+++ src/components/auxiliares/DashboardNavbar.js
@@ -1,6 +1,14 @@
 import { useState } from 'react';
-import { Typography, AppBar, Toolbar, Box, IconButton, Badge, Avatar } from '@mui/material';
-import { ExitToApp } from '@mui/icons-material';
+import {
+  Typography,
+  AppBar,
+  Toolbar,
+  Box,
+  IconButton,
+  Badge,
+  Avatar,
+} from '@mui/material';
+import { ExitToApp, Menu } from '@mui/icons-material';
 import { Link, useLocation } from 'react-router-dom';
 import Logo from 'src/components/auxiliares/Logo.js';
 import { sideBarOptions } from 'src/components/auxiliares/sideBarOptions.js';
@@ -16,23 +24,21 @@ export default function DashboardNavbar({
   idSociety,
   loggedUser,
 }) {
-
   const navigate = useNavigate();
-  
+
   const { pathname } = useLocation();
 
   // const lastPartOfPath = pathname.split('/').at(-1);
   const lastPartOfPath = pathname.split('/')[pathname.split('/').length - 1];
 
   const nameOfPage =
-    sideBarOptions.find(option => option.path === lastPartOfPath)?.title || lastPartOfPath;
-
+    sideBarOptions.find(option => option.path === lastPartOfPath)?.title ||
+    lastPartOfPath;
 
   const [notifications] = useState([]);
   function logOut() {
     setLoggedUser(null);
-    navigate(`../${idSociety?.nombre}`)
-
+    navigate(`../${idSociety?.nombre}`);
   }
   return (
     <AppBar elevation={0}>
@@ -40,19 +46,22 @@ export default function DashboardNavbar({
         <Link to="/">
           <Logo idSociety={idSociety} />
         </Link>
-            <div style={{ marginLeft: 50, fontSize: 24 }}>
-              
-              <Typography align="left" color="white" variant="h4">
-              {nameOfPage.replace("-"," de ").replace("%20"," ").replace("%20"," ").replace("%20"," ").replace("_",": ")}
-              </Typography> 
-
-            </div>
+        <div style={{ marginLeft: 50, fontSize: 24 }}>
+          <Typography align="left" color="white" variant="h4">
+            {nameOfPage
+              .replace('-', ' de ')
+              .replace('%20', ' ')
+              .replace('%20', ' ')
+              .replace('%20', ' ')
+              .replace('_', ': ')}
+          </Typography>
+        </div>
         <Box sx={{ flexGrow: 1 }} />
-               
+
         <div>
-            <Typography align="left" color="white" variant="h4">
-                {loggedUser?.user}
-            </Typography>        
+          <Typography align="left" color="white" variant="h4">
+            {loggedUser?.user}
+          </Typography>
         </div>
 
         <IconButton color="inherit">
@@ -66,6 +75,13 @@ export default function DashboardNavbar({
               height: 30,
             }}
           />
+        </IconButton>
+        <IconButton
+          color="inherit"
+          sx={{ display: { md: 'block', lg: 'none' } }}
+          onClick={setisMobileNavOpen}
+        >
+          <Menu />
         </IconButton>
         <IconButton color="inherit" onClick={logOut}>
           <ExitToApp />
